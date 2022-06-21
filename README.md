7841454a91e1673a8e5aa40cab820a0432cad1df
poohoot-play <poohoot.play@gmail.com>
2022-06-21 13:56:22 +0900

    useState를 여러 번 사용한 Info 추가

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # React TypeScript Hooks
+
+- useState
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
-import Counter from "./Counter";
+import Counter from "./Counter"; // #1 useState 하나 사용
+import Info from "./Info"; // #2 useState 여러번 사용
 
 const App = () => {
   return (
-    <Counter/>
+    <>
+      <Counter/> 
+      <Info/>
+    </>
   );
 }
 
